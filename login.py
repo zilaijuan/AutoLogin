4e489ee3004ab82afd227b6d151496e281d22ffe
zilaijuan <1823663710@qq.com>
2018-09-11 16:23:20 +0800

    modify

--- login.py
+++ login.py
@@ -11,6 +11,7 @@ from re import match
 from os import path
 from ConfigParser import ConfigParser
 from argparse import ArgumentParser
+import urlparse
 #from configobj import ConfigObj
 
 username=''
@@ -44,7 +45,7 @@ def readConf():
     return flag
 
 def login():
-    sina = "https://www.sina.com.cn"
+    sina = "http://www.sina.com"
     portal = "https://portalnew.dhu.edu.cn/"
     headers = {
             'Accept':'text/html,application/xhtml+xm…plication/xml;q=0.9,*/*;q=0.8',
@@ -56,21 +57,40 @@ def login():
             'User-Agent':'Mozilla/5.0 (Windows NT 6.1; W…) Gecko/20100101 Firefox/57.0'
             }
 
-    s = sessions.Session()
+    s = sessions.Session() 
     sina = s.get(sina,allow_redirects=False)
     sina.encoding = 'UTF-8-SIG'
-    #print(sina.text)
-    if match(".*DOCTYPE.*", sina.text) == None:
+#    print(sina.text)
+    if match(".*html.*", sina.text) == None:
         switch_hm1 = s.get(sina.headers['Location'],allow_redirects=False)
 #        print(switch_hm1.headers)
         switchphp = s.get(portal+switch_hm1.headers['Location'],allow_redirects=False)
 #        print(switchphp.cookies.get_dict())
         ck = switchphp.cookies
+#        print(ck)
         loginInfo = {'username':username,'password':password}
         #ck = {"PHPSESSID":"t25bao5qm3219mndrhdltgo455"}
-        resp = s.post('https://portalnew.dhu.edu.cn/post.php',data = loginInfo,cookies=ck)
-        resp.encoding = 'UTF-8-SIG'
-        print(resp.text)
+#        print(username)
+#        print(password)
+        resp = s.post('https://portalnew.dhu.edu.cn/post.php',data = loginInfo,cookies=ck,allow_redirects=False)
+        resp.encoding = 'UTF-8'
+#        print(resp.text)
+#        print(resp.headers)
+        h = resp.headers
+        h = dict(h)
+#        print(h)
+#        print(type(h))
+#        print(h)
+#        print(h['location'])
+        
+        query = resp.headers['Location']
+        params = urlparse.parse_qs(query.split('?')[1],True)
+#        print(params)
+        if(params['p'][0] == 'status'):
+            print("Success! ")
+        else:
+            print(params['error'][0])
+        
     else:
         print("Already connect to Internet!")
     
